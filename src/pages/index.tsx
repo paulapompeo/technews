436e68c1a1f81065ebb4adc1c1d8445bd0323ad2
Paula Pompeo <paula.pompeo@macaiapp.com>
2022-08-10 22:49:25 +0200

    adds stripe api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,17 @@
+import { GetServerSideProps } from "next"
 import Head from "next/head"
+import { SubscribeButton } from "../components/SubscribeButton"
+import { stripe } from "../services/stripe"
 import styles from "./home.module.scss"
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head><title>Home | Tech News</title></Head>
@@ -12,12 +22,34 @@ export default function Home() {
           <h1>News about the <span>Tech</span> world.</h1>
           <p>
             Get access to all the publications <br />
-            <span>for €9,90/month</span>
+            <span>for {product.amount}/month</span>
           </p>
+          <SubscribeButton priceId={product.priceId} />
         </section>
 
         <img src="/images/avatar.svg" alt="Girl coding" />
       </main>
     </>
   )
+}
+
+// call via SSR
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve("price_1LVKLdGS7mXz9pA0qdDDxV8z", {
+    expand: ["product"]
+  })
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('it-IT', {
+      style: "currency",
+      currency: "EUR"
+    }).format(price.unit_amount / 100)
+  }
+
+  return {
+    props: {
+      product
+    }
+  }
 }
