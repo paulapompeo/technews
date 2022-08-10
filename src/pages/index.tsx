06266af1279581f690b836ba6efb1cdbbd29a1fa
Paula Pompeo <paula.pompeo@macaiapp.com>
2022-08-10 20:30:53 +0200

    apply changes on main

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,23 @@
 import Head from "next/head"
+import styles from "./home.module.scss"
 
 export default function Home() {
   return (
     <>
-      <Head><title>Tech News | Home</title></Head>
-      <h1>Ciao</h1>
+      <Head><title>Home | Tech News</title></Head>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>✌️ Hey, welcome</span>
+          <h1>News about the <span>Tech</span> world.</h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for €9,90/month</span>
+          </p>
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   )
 }
