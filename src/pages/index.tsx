514315fec072779108431e9ea77498a89ec0493b
Paula Pompeo <paula.pompeo@macaiapp.com>
2022-08-10 23:04:26 +0200

    changes to SSG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,7 @@
-import { GetServerSideProps } from "next"
+import {
+  // GetServerSideProps,
+  GetStaticProps
+} from "next"
 import Head from "next/head"
 import { SubscribeButton } from "../components/SubscribeButton"
 import { stripe } from "../services/stripe"
@@ -10,6 +13,11 @@ interface HomeProps {
     amount: number;
   }
 }
+
+// NEXT -> ways to call api
+// Client-side: depends on user action
+// Server-side: dynamic data from user (user session)
+// Static side generation: blog, product on e-commerce (google index)
 
 export default function Home({ product }: HomeProps) {
   return (
@@ -33,8 +41,29 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
-// call via SSR
-export const getServerSideProps: GetServerSideProps = async () => {
+// SSR -> dinamic
+// export const getServerSideProps: GetServerSideProps = async () => {
+//   const price = await stripe.prices.retrieve("price_1LVKLdGS7mXz9pA0qdDDxV8z", {
+//     expand: ["product"]
+//   })
+
+//   const product = {
+//     priceId: price.id,
+//     amount: new Intl.NumberFormat('it-IT', {
+//       style: "currency",
+//       currency: "EUR"
+//     }).format(price.unit_amount / 100)
+//   }
+
+//   return {
+//     props: {
+//       product
+//     }
+//   }
+// }
+
+// SSG -> static (if all users see the same thing)
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve("price_1LVKLdGS7mXz9pA0qdDDxV8z", {
     expand: ["product"]
   })
@@ -50,6 +79,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product
-    }
+    },
+    revalidate: 60 * 60 * 24 // 24hrs
   }
 }
